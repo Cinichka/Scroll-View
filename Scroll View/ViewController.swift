80a794915d84cfc1fd5a030d2cd2467d17cd82dc
cinichka85 <cinichka85@gmail.com>
2018-10-08 09:32:22 +0300

    first commit

--- Scroll View/ViewController.swift
+++ Scroll View/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	class ViewController: UIViewController, UIScrollViewDelegate {
+		
+		@IBOutlet weak var imageView: UIImageView!
+		@IBOutlet weak var scrollView: UIScrollView!
+
+		
+		
+		override func viewDidLoad() {
+			super.viewDidLoad()
+			scrollView.maximumZoomScale = 20
+			scrollView.minimumZoomScale = 1
+			scrollView.delegate = self
+			// Do any additional setup after loading the view.
+		}
+		
+		
+		func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+			return imageView
+		}
+		
+		/*
+		// MARK: - Navigation
+		// In a storyboard-based application, you will often want to do a little preparation before navigation
+		override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		// Get the new view controller using segue.destination.
+		// Pass the selected object to the new view controller.
+		}
+		*/
+		
 	}
 
-
-}
 
